f5f1c1eadd3b5d1c990d06f8d65b8c5b77c2a7d8
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-10-16 09:49:12 -0300

    Instalando Tailwind CSS

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      "theme-one": "#340b4d",
+      "theme-two": "#171235",
+      "theme-three": "#e0c512",
+      "theme-four": "#e08c0f",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
