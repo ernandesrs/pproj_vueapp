996ad87df5bbe9881b93837945568383029fee7e
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-08-02 19:07:34 -0400

    Creating custom layout

---
+++ src/components/layouts/MainLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div>Header - Main Layout</div>
+    <div>
+      <router-view />
+    </div>
+    <div>Footer - Main Layout</div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
