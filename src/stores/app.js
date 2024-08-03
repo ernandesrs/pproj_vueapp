56c6e86d9cb3e511fe089a4005e89ed29f8d43bd
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-08-03 09:20:28 -0400

    App store

---
+++ src/stores/app.js
@@ -0,0 +1,40 @@
+import { reactive } from 'vue'
+import { defineStore } from 'pinia';
+
+const MOBILE_SIZE = 1024;
+
+export const useAppStore = defineStore('app', () => {
+  const stateData = reactive({
+    inMobile: window.innerWidth <= MOBILE_SIZE,
+    windowWidth: window.innerWidth
+  });
+
+  /**
+   * Check if current window width is from mobile
+   */
+  const inMobileCheck = () => {
+    if (stateData.windowWidth <= MOBILE_SIZE && !stateData.inMobile) {
+      stateData.inMobile = true;
+    } else if (stateData.windowWidth > MOBILE_SIZE && stateData.inMobile) {
+      stateData.inMobile = false;
+    }
+  };
+
+  /**
+   * Add a window resize event monitor and
+   * check if window width is from mobile
+   */
+  const windowResizeAddEvent = () => {
+    window.addEventListener('resize', () => {
+      stateData.windowWidth = window.innerWidth;
+      inMobileCheck();
+    });
+  };
+
+  return {
+    stateData,
+
+    inMobileCheck,
+    windowResizeAddEvent
+  }
+});
