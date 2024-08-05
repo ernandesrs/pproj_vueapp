3b9b9a51a2b3592203c9cbe284dc8ca0c153f8c0
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-08-05 11:38:24 -0400

    Outlined buttons

--- src/components/ButtonElem.vue
+++ src/components/ButtonElem.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class="flex justify-center items-center gap-3 shadow duration-300 disabled:pointer-events-none disabled:bg-opacity-75 disabled:text-opacity-75"
+    class="flex justify-center items-center gap-3 duration-300 disabled:pointer-events-none disabled:text-opacity-75 border border-transparent"
     :class="getClass"
   >
     <icon-elem
@@ -63,9 +63,9 @@ const props = defineProps({
   },
 
   /**
-   * Variations: allows filled, outlined and text
+   * Variants: allows filled, outlined and text
    */
-  variation: {
+  variant: {
     type: String,
     default: 'filled'
   },
@@ -97,14 +97,40 @@ const props = defineProps({
 
 const getClass = computed(() => {
   const colors = {
-    primary: 'bg-purple-700 text-zinc-100 hover:bg-purple-800',
-    secondary: 'bg-fuchsia-700 text-zinc-100 hover:bg-fuchsia-800',
-    success: 'bg-emerald-700 text-zinc-100 hover:bg-emerald-800',
-    info: 'bg-sky-700 text-zinc-100 hover:bg-sky-800',
-    danger: 'bg-rose-700 text-zinc-100 hover:bg-rose-800',
-    warning: 'bg-yellow-600 text-zinc-100 hover:bg-yellow-700',
-    dark: 'bg-zinc-700 text-zinc-100 hover:bg-zinc-800',
-    light: 'bg-zinc-100 text-zinc-500 hover:bg-zinc-200'
+    primary: {
+      filled: 'bg-purple-700 text-zinc-100 hover:bg-purple-800',
+      outlined: 'border-purple-700 text-purple-700 hover:border-purple-900 hover:text-purple-900'
+    },
+    secondary: {
+      filled: 'bg-fuchsia-700 text-zinc-100 hover:bg-fuchsia-800',
+      outlined:
+        'border-fuchsia-700 text-fuchsia-700 hover:border-fuchsia-900 hover:text-fuchsia-900'
+    },
+    success: {
+      filled: 'bg-emerald-700 text-zinc-100 hover:bg-emerald-800',
+      outlined:
+        'border-emerald-700 text-emerald-700 hover:border-emerald-900 hover:text-emerald-900'
+    },
+    info: {
+      filled: 'bg-sky-700 text-zinc-100 hover:bg-sky-800',
+      outlined: 'border-sky-700 text-sky-700 hover:border-sky-900 hover:text-sky-900'
+    },
+    danger: {
+      filled: 'bg-rose-700 text-zinc-100 hover:bg-rose-800',
+      outlined: 'border-rose-700 text-rose-700 hover:border-rose-900 hover:text-rose-900'
+    },
+    warning: {
+      filled: 'bg-yellow-600 text-zinc-100 hover:bg-yellow-700',
+      outlined: 'border-yellow-600 text-yellow-600 hover:border-yellow-900 hover:text-yellow-900'
+    },
+    dark: {
+      filled: 'bg-zinc-700 text-zinc-100 hover:bg-zinc-800',
+      outlined: 'border-zinc-500 text-zinc-500 hover:border-zinc-800 hover:text-zinc-800'
+    },
+    light: {
+      filled: 'bg-zinc-100 text-zinc-500 hover:bg-zinc-200',
+      outlined: 'border-zinc-300 text-zinc-400 hover:border-zinc-400 hover:text-zinc-500'
+    }
   }
   const sizes = {
     sm: (props?.icon ? 'w-9 h-9' : 'h-9 px-5') + ' text-sm',
@@ -114,9 +140,10 @@ const getClass = computed(() => {
   return (
     sizes[props.size] +
     ' ' +
-    colors[props.color] +
+    colors[props.color][props.variant] +
     (props.circle ? ' rounded-full' : ' rounded-lg') +
-    (props.loading ? ' animate-pulse pointer-events-none' : '')
+    (props.loading ? ' animate-pulse pointer-events-none' : '') +
+    (props.variant != 'filled' ? ' disabled:border-opacity-50' : ' disabled:bg-opacity-75 shadow')
   )
 })
 </script>
