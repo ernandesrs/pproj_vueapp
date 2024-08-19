63d1476aef91605de0483cd9347500095e5eab06
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-08-19 17:37:17 -0400

    Starting implementation of a route access guard

---
+++ src/core/guards/access.js
@@ -0,0 +1,29 @@
+import cookies from "../plugins/cookies"
+
+const getAuthToken = () => {
+    return cookies.get('auth_token')
+}
+
+export const onlyAuthenticated = (to, from, next) => {
+    let authToken = getAuthToken()
+    let route = null
+
+    if (!authToken) {
+        route = {
+            name: 'auth.login'
+        }
+    }
+
+    next(route)
+}
+
+export const onlyUnauthenticated = (to, from, next) => {
+    let authToken = getAuthToken()
+    let route = null
+
+    if (authToken) {
+        console.log('is auth')
+    }
+
+    next(route)
+}
