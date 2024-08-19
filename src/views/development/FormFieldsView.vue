65fcc615c2fad0e141ca213f3eac47ba641bb319
Ernandes R Souza <ernandesrsouza@gmail.com>
2024-08-19 15:29:02 -0400

    Correction

--- src/views/development/FormFieldsView.vue
+++ src/views/development/FormFieldsView.vue
@@ -5,54 +5,6 @@
     page-subtitle="Veja exemplos de campos de formulário"
     :loading-content="loadingContent"
   >
-    <card-section bordered title="Form" subtitle="Lorem dolor" class="col-span-12 lg:col-span-8">
-      <template #content>
-        <div class="py-3">
-          <base-form :on-submit="submitPromise" :on-clear="clearPromise">
-            <div class="grid grid-cols-12 gap-5">
-              <field-form
-                class="col-span-12 sm:col-span-6"
-                v-model="form.name"
-                type="text"
-                label="Nome"
-              />
-              <field-form
-                class="col-span-12 sm:col-span-6"
-                v-model="form.email"
-                type="email"
-                label="E-mail"
-              />
-              <field-form
-                class="col-span-12 sm:col-span-6"
-                v-model="form.age"
-                type="number"
-                label="Idade"
-              />
-              <field-form
-                class="col-span-12 sm:col-span-6"
-                v-model="form.gender"
-                type="select"
-                label="Gênero"
-                :options="[
-                  {
-                    label: 'Masculino',
-                    value: 'm'
-                  },
-                  {
-                    label: 'Faminino',
-                    value: 'f'
-                  }
-                ]"
-              />
-              <div class="col-span-12 mb-3 bg-zinc-100 border p-4">
-                {{ form }}
-              </div>
-            </div>
-          </base-form>
-        </div>
-      </template>
-    </card-section>
-
     <card-section
       bordered
       title="Fields"
@@ -62,15 +14,25 @@
       <!--  -->
       <template #content>
         <div class="flex flex-col gap-y-4 py-4">
-          <field-form v-model="fields.text" label="Text type" type="text" />
+          <field-form name="text_field" v-model="fields.text" label="Text type" type="text" />
 
-          <field-form v-model="fields.number" label="Number type" type="number" />
+          <field-form
+            name="number_field"
+            v-model="fields.number"
+            label="Number type"
+            type="number"
+          />
 
-          <field-form v-model="fields.password" label="Password type" type="password" />
+          <field-form
+            name="password_field"
+            v-model="fields.password"
+            label="Password type"
+            type="password"
+          />
 
-          <field-form v-model="fields.date" label="Date type" type="date" />
+          <field-form name="date_field" v-model="fields.date" label="Date type" type="date" />
 
-          <field-form v-model="fields.file" label="File type" type="file" />
+          <field-form name="file_field" v-model="fields.file" label="File type" type="file" />
         </div>
       </template>
     </card-section>
@@ -84,6 +46,7 @@
       <!--  -->
       <template #content>
         <field-form
+          name="select_field"
           v-model="fields.select"
           label="Select type"
           type="select"
@@ -118,18 +81,21 @@
       <template #content>
         <div class="flex flex-col gap-y-4 py-4">
           <field-form
+            name="field_text_feedback"
             v-model="fields.text"
             label="Default feedback"
             :feedback="fields.errors.with_feedback"
           />
 
           <field-form
+            name="field_text_success_feedback"
             v-model="fields.text"
             label="Success feedback"
             :success-feedback="fields.errors.with_feedback"
           />
 
           <field-form
+            name="field_text_fail_feedback"
             v-model="fields.text"
             label="Fail feedback"
             :fail-feedback="fields.errors.with_feedback"
@@ -152,7 +118,6 @@
 import CardSection from '@/components/card/CardSection.vue'
 import MainViewBase from '@/components/layouts/main/MainViewBase.vue'
 import FieldForm from '@/components/form/FieldForm.vue'
-import BaseForm from '@/components/form/BaseForm.vue'
 import { useAppStore } from '@/stores/app'
 import { reactive, ref } from 'vue'
 
@@ -174,39 +139,12 @@ const fields = reactive({
   }
 })
 
-const form = reactive({
-  name: null,
-  email: null,
-  age: null,
-  gender: null
-})
-
 /**
  *
  * Calls
  *
  */
 appStore.updatePageTitle('Form fields')
-
-const submitPromise = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve()
-    }, 2000)
-  })
-}
-
-const clearPromise = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      form.name = null
-      form.email = null
-      form.age = null
-      form.gender = null
-      resolve()
-    }, 1000)
-  })
-}
 
 setTimeout(() => {
   loadingContent.value = false
